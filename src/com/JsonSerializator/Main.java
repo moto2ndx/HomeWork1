5f7670ae100e74dcb5ea96907fd62a68b816cc56
moto2ndx <moto2ndx@gmail.com>
2022-04-24 17:08:43 +0300

    add homework 2

---
+++ src/com/JsonSerializator/Main.java
@@ -0,0 +1,12 @@
+package com.JsonSerializator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user = new User(1,"Oleg",22);
+
+        System.out.println(user);
+
+    }
+}
